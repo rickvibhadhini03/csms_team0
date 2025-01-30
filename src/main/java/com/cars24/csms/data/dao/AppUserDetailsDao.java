67e95048ae34d447d886af926134a977c5122316
smriti <smritiraj1504@gmail.com>
2025-01-30 14:57:31 +0530

    i cant

--- src/main/java/com/cars24/csms/data/dao/AppUserDetailsDao.java
+++ src/main/java/com/cars24/csms/data/dao/AppUserDetailsDao.java
@@ -1,29 +1,13 @@
 package com.cars24.csms.data.dao;
 
-<<<<<<< HEAD
-import com.cars24.csms.data.entities.AppUserDetails;
-import com.cars24.csms.data.req.LoginReq;
-=======
 import com.cars24.csms.data.req.LoginReq;
 import com.cars24.csms.data.req.SignUpReq;
->>>>>>> fa380fb6e3222d72ff9983df3de66a1d21cc313f
 import org.springframework.stereotype.Service;
 
 @Service
 public interface AppUserDetailsDao {
-<<<<<<< HEAD
-    public AppUserDetails getAppUser(LoginReq loginReq);
-    //public boolean findUserByUsername(String username);
-    boolean userExists(String username);
-    void saveUser(AppUserDetails user);
-
-
-}
-
-=======
     String getUserDetails(LoginReq loginReq);
     void RegisterUser(SignUpReq signUpRequest);
     Boolean checkIfUserExists(SignUpReq signUpRequest);
 
 }
->>>>>>> fa380fb6e3222d72ff9983df3de66a1d21cc313f
--- src/main/java/com/cars24/csms/data/entities/AppUserDetails.java
+++ src/main/java/com/cars24/csms/data/entities/AppUserDetails.java
@@ -2,26 +2,6 @@ package com.cars24.csms.data.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
-<<<<<<< HEAD
-@Entity
-@Table(name="userdetails")
-@Data
-public class AppUserDetails {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String username;
-    private String password;
-    private boolean isEnabled;
-
-    public boolean isEnabled() {
-        return isEnabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        isEnabled = enabled;
-    }
-=======
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
@@ -50,5 +30,4 @@ public class AppUserDetails {
 
 
 
->>>>>>> fa380fb6e3222d72ff9983df3de66a1d21cc313f
 }
